3297bd2bb3c2d021f06048f8ec973e467f1dc2a2
mat3jw <vlcekmatej2007@gmail.com>
2024-10-01 09:37:40 +0200

    postupnosti

---
+++ cislo_N.py
@@ -0,0 +1,21 @@
+N = int(input("zadajte cislo N (prirodzene): "))
+
+for i in range(1, N + 1):
+    print(i, end=', ')
+    
+print("\n")
+    
+for i in range(2, N + 1):
+    print(i, end=', ')
+
+print("\n")
+
+for i in range(N, 0, -1):
+    print(i, end=', ')
+
+print("\n")
+    
+for i in range(1, N + 1, 2):
+    print(i, end=', ')
+    
+    
---
+++ postupnosti.py
@@ -0,0 +1,15 @@
+for i in range(0, 101):
+    print(i, -i, end=', ')
+ 
+print("\n")
+
+for i in range(0, 101, 2):
+    print(, end=', ')
+    
+print("\n")
+
+# for i in range(1, 1048577):
+#     print(i**2)
+
+
+    
