a3a651776d0e28f70b807ad937ad7f88ea2d77e7
mat3jw <158332973+mat3jw@users.noreply.github.com>
2024-11-26 09:14:44 +0100

    Update znamky.py

--- lists/znamky.py
+++ lists/znamky.py
@@ -2,12 +2,3 @@ meno = input('zadajte meno ziaka: ')
 predmet = input('zadajte predmet: ')
 znamky = [int(znamka) for znamka in input('zadajte znamky: ').split()]
 average = sum(znamky) / len(znamky)
-
-print(meno ,',', predmet, ':')
-print(znamky)
-
-if znamka < 0 or znamka > 6:
-    print('neplatne znamky')
-
-?
-
