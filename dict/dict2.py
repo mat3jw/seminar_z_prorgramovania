07b6fc4483754aa36ed705be21cb980337b851a5
mat3jw <vlcekmatej2007@gmail.com>
2025-02-18 08:46:34 +0100

    commit

---
+++ dict/dict2.py
@@ -0,0 +1,9 @@
+text = "pocitac"
+
+morzeovka = {"a": ".-", "b": "-..", "c": ".-."}
+z_morzeovky = {".-":"a", "-..":"b",".-.":"c"}
+
+clovek = {"meno": "Fero",
+          "priezvisko": "Mrkvicka",
+          "adresa": "Zahradna 123",
+          "email":  "fero.mrkvicka@gmail.com"}
