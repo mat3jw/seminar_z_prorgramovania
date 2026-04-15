2f05e4996b29749990bbee7c401884d1619f0d44
mat3jw <vlcekmatej2007@gmail.com>
2026-04-15 11:39:52 +0200

    tk

--- maturitne_ulohy/davka_4/4_1.py
+++ maturitne_ulohy/davka_4/4_1.py
@@ -6,6 +6,8 @@ stvorcekov = len(letters)
 sirka = 50
 medzera = 20
 
+vyhra = choice(letters)
+
 sirka_okna = stvorcekov * sirka + (stvorcekov + 1) * medzera
 vyska_okna = sirka + 2 * medzera
 
@@ -15,7 +17,19 @@ canvas.pack()
 
 x,y = medzera, medzera
 for i in range(stvorcekov):
-    farba = "#%06x" % randint(0, 0xFFFFFF)
-    canvas.create_rectangle(x, y, x + sirka, y + sirka, fill=farba)
-    canvas.create_text(x + sirka / 2, y + sirka / 2, text=letters[i], font=("Arial", 20))
+    tagy = ''
+    if letters[i] == vyhra:
+        tagy = "vyherny_stvorec"
+
+    canvas.create_rectangle(x, y, x + sirka, y + sirka, fill="green", tags=tagy)
+    canvas.create_text(x + sirka / 2, y + sirka / 2, text=letters[i], font=("Arial", 20), tags=tagy)
     x += sirka + medzera
+
+def kruzok(suradnice):
+    canvas.delete("all")
+    canvas.create_text(sirka_okna / 2, vyska_okna / 2, text="Vyhral si!")
+    utvar = canvas.find.withtag("current")
+    canvas.itemconfig(utvar, fill="red")
+
+canvas.tag_bind("vyherny_stvorec", "<Button-1>", kruzok)
+window.mainloop()  
