391e7dd681c21581f66e4a925e64e79df6a1216e
mat3jw <vlcekmatej2007@gmail.com>
2026-05-11 10:05:10 +0200

    practice na maturu 1

--- maturitne_ulohy/davka_1/1A_m.py
+++ maturitne_ulohy/davka_1/1A_m.py
@@ -5,15 +5,15 @@ with open("sklenik.txt") as file:
         print(riadok)
         for i in range(len(riadok)):
             riadok[i] = int(riadok[i])
-        print(riadok)   
+            print(riadok)
         tabulka.append(riadok)
     print(tabulka)
 
-    print(f"Počet riadkov: {len(tabulka)}")
-    print(f"Počet stĺpcov: {len(tabulka[0])}")
+    print(f"pocet riadkov: {len(tabulka)}")
+    print(f"pocet stlpcov: {len(tabulka[0])}")
 
     max_teplota = min_teplota = tabulka[0][0]
-    min_pos = max_pos = (0,0)
+    max_pos = min_pos = (0,0)
 
     for r in range(len(tabulka)):
         for s in range(len(tabulka[r])):
@@ -23,16 +23,18 @@ with open("sklenik.txt") as file:
             if tabulka[r][s] < min_teplota:
                 min_teplota = tabulka[r][s]
                 min_pos = (r,s)
-    print(f"Maximum: {max_teplota} (riadok {max_pos[0] + 1} stlpec {max_pos[1] + 1})")
-    print(f"Minimum: {min_teplota} (riadok {min_pos[0] + 1} stlpec {min_pos[1] + 1})")
+    print(f"maximum: {max_teplota} (riadok {max_pos[0] + 1} stlpec {max_pos[1] + 1})")
+    print(f"minimum: {min_teplota} (riadok {min_pos[0] + 1} stlpec {min_pos[1] + 1})")
 
     suma = 0
     pocet = 0
+
     for riadok in tabulka:
         suma += sum(riadok)
         pocet += len(riadok)
-    priemer = suma / pocet
-    print(f"Priemer: {priemer}")
+    priemer = suma/pocet
+    print(f"priemer: {priemer}")
+
     nad_priemerom = pod_priemerom = nad_prahom = 0
     for riadok in tabulka:
         for cislo in riadok:
@@ -42,7 +44,6 @@ with open("sklenik.txt") as file:
                 pod_priemerom += 1
             if cislo > priemer + 15:
                 nad_prahom += 1
-        
-    print(f"Nad priemerom: {nad_priemerom}")
-    print(f"Pod priemerom: {pod_priemerom}")
-    print(f"Nad prahom: {nad_prahom}")
+    print(f"nad prahom: {nad_prahom}")
+    print(f"nad_priemerom: {nad_priemerom}")
+    print(f"pod_priemerom: {pod_priemerom}")
